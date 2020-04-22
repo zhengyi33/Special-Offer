9e103e7375b4f34c1bdb5f5e3f10b5ffc063c86e
zhengyi33 <52870340+zhengyi33@users.noreply.github.com>
2020-04-22 13:25:01 -0500

    Update MapsActivity.java

--- SpecialOffer/app/src/main/java/com/yizheng/specialoffer/MapsActivity.java
+++ SpecialOffer/app/src/main/java/com/yizheng/specialoffer/MapsActivity.java
@@ -100,15 +100,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         return mMap;
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
@@ -127,10 +119,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setupZoomListener();
         mMap.animateCamera(CameraUpdateFactory.zoomTo(16));
 
-        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        
     }
 
     @Override
@@ -218,11 +207,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         circles.clear();
     }
 
-//    private static void drawFences() {
-//        for (String id : FenceManager.getFences().keySet()){
-//            drawFence(FenceManager.getFences().get(id));
-//        }
-//    }
+
 
     void drawFence(FenceData fd) {
         int line = Color.parseColor(fd.getFenceColor());
